59e74df43ce4eeae45791b8ab38bee813e68e301
unknown <moshiur.mirage@gmail.com>
2023-10-28 16:32:31 +0600

    booking api,service id etc done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const cors=require('cors');
-const app=express();
-const port=process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 //middleware
 app.use(cors());
@@ -23,14 +23,48 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const serviceCollection = client.db('CarDoctor').collection('Services');
+    const bookingCollection = client.db('CarDoctor').collection('Bookings')
+
+    app.get('/services', async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const options = {
+        projection: {
+          _id: 1,
+          title: 1,
+          price: 1,
+          img: 1
+        }
+      }
+      const result = await serviceCollection.findOne(query, options)
+      res.send(result);
+    })
+
+
+
+    app.post('/bookings',async(req,res)=>{
+      const booking=req.body;
+      console.log(booking);
+      const result=await bookingCollection.insertOne(booking);
+      res.send(result)
+    })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -42,9 +76,9 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send('doctor is running')
+app.get('/', (req, res) => {
+  res.send('doctor is running')
 })
-app.listen(port,()=>{
-    console.log(`car doctor server is running on port ${port}`)
+app.listen(port, () => {
+  console.log(`car doctor server is running on port ${port}`)
 })
