0e6fb41850fdb1f3edfa91869376fb547e60630c
unknown <moshiur.mirage@gmail.com>
2023-10-28 19:00:12 +0600

    done without jwt

--- index.js
+++ index.js
@@ -60,6 +60,44 @@ async function run() {
     })
 
 
+    app.delete('/bookings/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query)
+      res.send(result)
+
+    })
+
+
+    app.get('/bookings', async (req, res) => {
+      let query={}
+      if (req.query?.email){
+        query={email:req.query.email}
+      }
+      const cursor = await bookingCollection.find(query).toArray();
+      res.send(cursor)
+    })
+
+
+    app.patch('/bookings/:id',async(req,res)=>{
+      const id=req.params.id;
+      const filter={_id:new ObjectId(id)};
+      const updateBooking=req.body;
+      console.log(updateBooking)
+      const updateDoc={
+        $set:{
+          status:updateBooking.status
+        },
+      };
+      const result = await bookingCollection.updateOne(filter,updateDoc);
+      res.send(result);
+      console.log(result)
+    })
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
